b3bf53aa467d3ab92b4419600942d91ec9f7ff1f
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-11-17 20:56:54 +0300

    style: add empty main page base styles

--- src/pages/main/ui/main.page.tsx
+++ src/pages/main/ui/main.page.tsx
@@ -2,10 +2,12 @@ import { useEvent, useStore } from 'effector-react'
 import { useEffect } from 'react'
 import { NoteModel } from 'src/entities/note'
 import { UserModel } from 'src/entities/user'
+import { Container } from 'src/shared/ui/container'
 import { Footer } from 'src/widgets/footer/ui'
 import { Header } from 'src/widgets/header/ui'
 
-import * as model from './model'
+import * as model from '../model'
+import styles from './main.module.scss'
 
 export function Main() {
   const handlePageLoad = useEvent(model.pageLoadEv)
@@ -34,11 +36,17 @@ export function Main() {
       <Header />
 
       <main>
-        {notes.length ? (
-          <div>Notes list</div>
-        ) : (
-          <div>You don't have notes yet</div>
-        )}
+        <Container>
+          {notes.length ? (
+            <div className={styles.contentContainer}>
+              <div>Notes list</div>
+            </div>
+          ) : (
+            <div className={styles.empty}>
+              <h1 className={styles.emptyText}>You don't have notes yet</h1>
+            </div>
+          )}
+        </Container>
       </main>
 
       <Footer />
