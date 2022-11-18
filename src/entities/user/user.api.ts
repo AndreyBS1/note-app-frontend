a381df1ed54d4153bac4e5ce214460a5268567a0
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-11-18 13:21:43 +0300

    fix: add errors and types to user api

--- src/entities/user/user.api.ts
+++ src/entities/user/user.api.ts
@@ -2,18 +2,22 @@ import { apiService, IUser } from 'src/shared/api'
 import { USERS_KEY } from 'src/shared/config'
 import { createUid } from 'src/shared/lib'
 
-export async function createUser(name: string) {
+export async function createUser(name: string): Promise<IUser> {
   const id = createUid()
   const user: IUser = { id, name }
   await apiService.save(USERS_KEY, user)
   return user
 }
 
-export function getUser() {
-  return apiService.get<IUser>(USERS_KEY)
+export async function getUser(): Promise<IUser> {
+  const user = await apiService.get<IUser>(USERS_KEY)
+  if (user == null) {
+    throw new Error('Seems like user does not exist')
+  }
+  return user
 }
 
-export async function updateUser(updatedUserName: string) {
+export async function updateUser(updatedUserName: string): Promise<IUser> {
   const user = await apiService.get<IUser>(USERS_KEY)
   if (user == null) {
     throw new Error('Seems like user does not exist')
@@ -23,6 +27,6 @@ export async function updateUser(updatedUserName: string) {
   return updatedUser
 }
 
-export async function deleteUser() {
+export async function deleteUser(): Promise<void> {
   return apiService.clear(USERS_KEY)
 }
