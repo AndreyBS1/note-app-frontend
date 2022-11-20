747bc94c90827ce20f187163ce00103e4091b1e3
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-11-20 18:53:37 +0300

    fix: take initial state var out of hook

--- src/pages/auth/model/use-user-form.hook.ts
+++ src/pages/auth/model/use-user-form.hook.ts
@@ -1,12 +1,12 @@
 import { IUser } from 'src/shared/api'
 import { createUid, useForm } from 'src/shared/lib'
 
-export function useUserForm() {
-  const initialFormState: IUser = {
-    id: createUid(),
-    name: '',
-  }
+const initialFormState: IUser = {
+  id: createUid(),
+  name: '',
+}
 
+export function useUserForm() {
   const { form, handleChange } = useForm(initialFormState)
 
   return { form, handleChange }
