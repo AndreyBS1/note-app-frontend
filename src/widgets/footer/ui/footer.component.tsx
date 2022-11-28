b08f5845768e941dfbaf53fab8fe1db94f950c84
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-11-28 17:13:00 +0300

    feat: update footer

--- src/widgets/footer/ui/footer.component.tsx
+++ src/widgets/footer/ui/footer.component.tsx
@@ -1,9 +1,41 @@
+import { Icon } from 'src/shared/ui/icon'
+
 import styles from './footer.module.scss'
+
+import githubIcon from 'assets/github.svg'
+import mailIcon from 'assets/mail.svg'
+import telegramIcon from 'assets/telegram.svg'
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2>Created by Andrey Babenkov</h2>
+      <h3>Created by Andrey Babenkov in 2022</h3>
+      <div className={styles.linkContainer}>
+        <a
+          href="mailto:andrey.babenkov.00@gmail.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className={styles.link}
+        >
+          <Icon src={mailIcon} alt="email link" />
+        </a>
+        <a
+          href="https://andreybs.t.me"
+          target="_blank"
+          rel="noreferrer noopener"
+          className={styles.link}
+        >
+          <Icon src={telegramIcon} alt="telegram link" />
+        </a>
+        <a
+          href="https://github.com/AndreyBS1"
+          target="_blank"
+          rel="noreferrer noopener"
+          className={styles.link}
+        >
+          <Icon src={githubIcon} alt="github link" />
+        </a>
+      </div>
     </footer>
   )
 }
